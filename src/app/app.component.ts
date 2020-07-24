ba699c89d26145db87ceebc11b8e99370a0f7874
NykyforukStanislav <stanislav.nykyforuk@SNykyforuk.local>
2020-07-24 12:18:27 +0300

    removed redundant code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'payment-app';
-}
+export class AppComponent {}
